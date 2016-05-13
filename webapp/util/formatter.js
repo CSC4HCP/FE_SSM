5b6f0285095b82d044dc919c02f3a2ea54519c16
Lily Li <lily.li11@sap.com>
2016-05-13 09:55:37 +0000

    lily0513

--- webapp/util/formatter.js
+++ webapp/util/formatter.js
@@ -9,5 +9,19 @@ ssms.util.formatter = {
 	        sResult = "Private";
 	    }
 	    return sResult;
+	},
+	formatDateTime: function(sValue) {
+	    var dDate,dTime;
+	    
+	        dDate = sValue.substring(0,sValue.indexOf("T"));
+	    dTime = sValue.substring(sValue.indexOf("T") +1,sValue.indexOf("+") );
+
+	    return dDate + " " +dTime;
+	},
+	formatDate: function(sValue){
+	    return sValue.substring(0,sValue.indexOf("T"));
+	},
+	formatTime: function(sValue) {
+	    return sValue.substring(sValue.indexOf("T") +1,sValue.indexOf("+") );
 	}
 };
