ea0317e237ab7bf23fd142d0ae7bb7ba842c61c9
Kay Du <kay.du@sap.com>
2016-05-18 08:49:03 +0000

    add view for all controls and set tooltip for header Logo

--- webapp/controller/control/UploadCollection.controller.js
+++ webapp/controller/control/UploadCollection.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"ssms/controller/BaseController",
+	"sap/m/MessageToast"
+], function(BaseController, MessageToast) {
 	"use strict";
 
-	return Controller.extend("ssms.controller.control.UploadCollection", {
+	return BaseController.extend("ssms.controller.control.UploadCollection", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
@@ -13,41 +14,37 @@ sap.ui.define([
 		onInit: function() {
 
 		},
-		
+
+		/**
+		 * @function
+		 * @name onFileChange
+		 * @description Event handler for change event
+		 * @param {sap.ui.base.Event} - oEvent The fired event.
+		 */
 		onFileChange: function(oEvent) {
-		    var oSource = oEvent.getSource();
+			var oSource = oEvent.getSource();
+
+			MessageToast.show("Event 'change' of " + oSource + "has been triggered.");
 		},
-		
+
+		/**
+		 * @function
+		 * @name onFileDelete
+		 * @description Event handler for fileDeleted event
+		 * @param {sap.ui.base.Event} - oEvent The fired event.
+		 */
 		onFileDelete: function(oEvent) {
-		    var oSource = oEvent.getSource();
+			var oSource = oEvent.getSource();
+
+			MessageToast.show("Event 'fileDeleted' of " + oSource + "has been triggered.");
+		},
+
+		onPressUploadButton: function() {
+		  //  var oUploadCollection = this.byId("ssmsuploadCollection");
+		    
+// 			oUploadCollection.setUploadUrl("/destinations/SSM_DEST/api/document/upload/1");
+// 			oUploadCollection.upload();
 		}
-
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf ssms.view.controls.view.UploadCollection
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf ssms.view.controls.view.UploadCollection
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf ssms.view.controls.view.UploadCollection
-		 */
-		//	onExit: function() {
-		//
-		//	}
-
 	});
 
 });
