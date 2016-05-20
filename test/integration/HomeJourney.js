a7076afe158983f8be28c71e4a95190b2188f8b2
Chi Zhang <chi.zhang01@sap.com>
2016-05-20 06:09:14 +0000

    Make Home Page OPA Test Faster

--- test/integration/HomeJourney.js
+++ test/integration/HomeJourney.js
@@ -12,43 +12,39 @@ sap.ui.require(
 			When.onTheHomePage.iPressThePersonalButton();
 
 			// Assertions
-			Then.onTheHomePage.iShouldSeeThePersonalInformation().
-			and.iTeardownMyAppFrame();
+			Then.onTheHomePage.iShouldSeeThePersonalInformation();
 		});
 
 		opaTest("Should see the Existed Sessions", function(Given, When, Then) {
 			// Arrangements
-			Given.iStartMyApp();
+
 
 			//Actions
 			When.onTheHomePage.iPressTheExistedSession();
 
 			// Assertions
-			Then.onTheHomePage.iShouldSeeTheExistedSessionList().
-			and.iTeardownMyAppFrame();
+			Then.onTheHomePage.iShouldSeeTheExistedSessionList();
 		});
 
-		opaTest("Should see the Create Session Page", function(Given, When, Then) {
-			// Arrangements
-			Given.iStartMyApp();
+
+
+		opaTest("Should see the Search Information", function(Given, When, Then) {
+
+			//Actions
+			When.onTheHomePage.iPressTheSearchField();
+
+			// Assertions
+			Then.onTheHomePage.iShouldSeeTheSeachInfo();
+		});
+		
+			opaTest("Should see the Create Session Page", function(Given, When, Then) {
+	
 
 			//Actions
 			When.onTheHomePage.iPressTheCreateSession();
 
 			// Assertions
 			Then.onTheHomePage.iShouldSeeTheCreateSessionPage().
-			and.iTeardownMyAppFrame();
-		});
-
-		opaTest("Should see the Search Information", function(Given, When, Then) {
-			// Arrangements
-			Given.iStartMyApp();
-
-			//Actions
-			When.onTheHomePage.iPressTheSearchField();
-
-			// Assertions
-			Then.onTheHomePage.iShouldSeeTheSeachInfo().
 			and.iTeardownMyAppFrame();
 		});
 
