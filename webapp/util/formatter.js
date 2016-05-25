001ea2102ccb20d47cd1eb9af6cf32e0f689c402
Lily Li <lily.li11@sap.com>
2016-05-25 09:42:22 +0000

    formatter path

--- webapp/util/formatter.js
+++ webapp/util/formatter.js
@@ -1,11 +1,9 @@
-jQuery.sap.declare("ssms.util.formatter");
+// jQuery.sap.declare("ssms.util.formatter");
 sap.ui.define([
-	"ssms/controller/BaseController"
-], function(BaseController) {
+], function() {
 	"use strict";
 
-	return BaseController.extend("ssms.util.formatter", {
-
+	return {
 		formatVisibility: function(sValue) {
 			var sResult;
 			if (sValue) {
@@ -15,16 +13,19 @@ sap.ui.define([
 			}
 			return sResult;
 		},
+		
 		formatDateTime: function(sValue) {
 			return new Date(sValue).toLocaleString();
 		},
+		
 		formatDate: function(sValue) {
 			var dDateTime = new Date(sValue).toLocaleString();
 			return dDateTime.substring(0, dDateTime.indexOf(","));
 		},
+		
 		formatTime: function(sValue) {
 			var dDateTime = new Date(sValue).toLocaleString();
 			return dDateTime.substring(dDateTime.indexOf(",") + 2);
 		}
-	});
+	};
 });
