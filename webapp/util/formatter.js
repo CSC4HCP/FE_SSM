755f2d6ae44b463976303fadd391e67e5d8ed57b
Lily Li <lily.li11@sap.com>
2016-05-25 09:21:55 +0000

    modify formatter

--- webapp/util/formatter.js
+++ webapp/util/formatter.js
@@ -1,24 +1,30 @@
 jQuery.sap.declare("ssms.util.formatter");
-ssms.util.formatter = {
-	formatVisibility: function(sValue) {
-	   var sResult;
-	    if(sValue){
-	        sResult = "Public";
-	    }
-	    else{
-	        sResult = "Private";
-	    }
-	    return sResult;
-	},
-	formatDateTime: function(sValue) {
-	    return new Date(sValue).toLocaleString();
-	},
-	formatDate: function(sValue){
-	    var dDateTime = new Date(sValue).toLocaleString();
-	    return dDateTime.substring(0,dDateTime.indexOf(","));
-	},
-	formatTime: function(sValue) {
-	    var dDateTime = new Date(sValue).toLocaleString();
-	    return dDateTime.substring(dDateTime.indexOf(",") +2);
-	}
-};
+sap.ui.define([
+	"ssms/controller/BaseController"
+], function(BaseController) {
+	"use strict";
+
+	return BaseController.extend("ssms.util.formatter", {
+
+		formatVisibility: function(sValue) {
+			var sResult;
+			if (sValue) {
+				sResult = "Public";
+			} else {
+				sResult = "Private";
+			}
+			return sResult;
+		},
+		formatDateTime: function(sValue) {
+			return new Date(sValue).toLocaleString();
+		},
+		formatDate: function(sValue) {
+			var dDateTime = new Date(sValue).toLocaleString();
+			return dDateTime.substring(0, dDateTime.indexOf(","));
+		},
+		formatTime: function(sValue) {
+			var dDateTime = new Date(sValue).toLocaleString();
+			return dDateTime.substring(dDateTime.indexOf(",") + 2);
+		}
+	});
+});
