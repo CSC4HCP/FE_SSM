7e62de62d4f7d38c50766ecee03dae612625e9db
Kay Du <kay.du@sap.com>
2016-06-08 07:12:30 +0000

    notification list page OPA

--- test/integration/NotificationListJourney.js
+++ test/integration/NotificationListJourney.js
@@ -14,31 +14,69 @@ sap.ui.require(
 			When.onNotificationListPage.iLookAtTheScreen();
 
 			// Assertions
-			Then.onNotificationListPage.iShouldSeeTheSplitContainer();
+			Then.onNotificationListPage.theSplitContainerShouldDisplay();
 		});
 
 		opaTest("Should see both the Master and Detail Page", function(Given, When, Then) {
 			// Assertions
-			Then.onNotificationListPage.iShouldSeeTheMasterPage()
-			.and.iShouldSeeTheDetailPage();
+			Then.onNotificationListPage.theMasterPageShouldDisplay()
+			.and.theDetailPageShouldDisplay();
 		});
 		
 		opaTest("Should see 2 items in the master page and the 'All' item selected", function(Given, When, Then) {
 			Then.onNotificationListPage.theMasterPageShouldHaveTwoEntries()
-			.and.theAllItemsShouldBeSelected();	
+			.and.theAllItemsShouldBeSelected()
+			.and.theUnreadItemsShouldBeUnselected();	
 		});
 		
-		opaTest("Should see 6 items in the detail page while 'All' item selected", function(Given, When, Then) {
-			Then.onNotificationListPage.theDetailPageShouldHaveAllEntries();
+		opaTest("Should see 6 items in the current detail page and unread notifications are marked", function(Given, When, Then) {
+			Then.onNotificationListPage.theCurrentDetailPageShouldBeAllNotifications()
+			.and.theDetailPageShouldHaveAllEntries()
+			.and.theUnreadNotificationsShouldBeMarked();
 		});
 		
-		opaTest("Should see item selected after click it", function(Give, When, Then) {
+		opaTest("Should see read notification deleted after click the close button and confirm", function(Given, When, Then) {
 			// Actions
-			When.onNotificationListPage.iPressOnItem();
+			When.onNotificationListPage.iPressOnCloseButtonOfReadNotification()
+			.and.iPressOnOKButtonInDialog();
+			
+			Then.onNotificationListPage.theReadNotificationShouldBeDeleted()
+			.and.theCounterInAllItemShouldChange()
+			.and.theCounterInUnreadItemShouldNotChange();
+		});
+		
+		opaTest("Should see item selected and current detail page change after click it", function(Give, When, Then) {
+			// Actions
+			When.onNotificationListPage.iPressOnUnreadItem();
 			
 			// Assertions
 			Then.onNotificationListPage.theOtherItemShouldNotBeSelected()
-			.and.theItemShouldBeSelected();
+			.and.theItemShouldBeSelected()
+			.and.theCurrentDetailPageShouldChange();
+		});
+		
+		opaTest("Should see 2 items in the current detail page and unread notifications are marked", function(Given, When, Then) {
+			Then.onNotificationListPage.theDetailPageShouldHaveTwoEntries()
+			.and.allNotificationsShouldBeMarkedAsUnread();
+		});
+		
+		opaTest("Should see unread notification deleted after click the close button and confirm", function(Given, When, Then) {
+			// Actions
+			When.onNotificationListPage.iPressOnAllItem()
+			.and.iPressOnCloseButtonOfUnreadNotification()
+			.and.iPressOnOKButtonInDialog();
+			
+			Then.onNotificationListPage.theUnreadNotificationShouldBeDeleted()
+			.and.theCounterInAllItemShouldChangeWhenDeleteUnread();
+		});
+		
+		opaTest("Should see the counter of 'Unread' item change after click it", function(Given, When, Then) {
+			// Actions
+			When.onNotificationListPage.iPressOnUnreadItem();
+			
+			Then.onNotificationListPage.theCounterInUnreadItemShouldChangeWhenDeleteUnread();
+			
+			Given.iTearDownMyApp();
 		});
 	}
 );
